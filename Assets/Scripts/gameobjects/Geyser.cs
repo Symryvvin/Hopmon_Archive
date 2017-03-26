437f6603218120b823841723ea6b398666429be2
Roman <nosferat@bk.ru>
2017-03-26 22:07:47 +0300

    Implement geyser

---
+++ Assets/Scripts/gameobjects/Geyser.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Geyser : MonoBehaviour {
+    public AudioClip geyserSound;
+    public float pause;
+
+    private float duration;
+
+
+    private GameObject steam;
+
+    void Awake() {
+        steam = transform.FindChild("Steam").gameObject;
+        steam.SetActive(false);
+        duration = geyserSound.length;
+    }
+
+    void Start() {
+        StartCoroutine(Eruption());
+    }
+
+    IEnumerator Eruption() {
+        while (true) {
+            yield return new WaitForSeconds(pause);
+            steam.SetActive(true);
+            AudioSource.PlayClipAtPoint(geyserSound, transform.position);
+            yield return new WaitForSeconds(duration);
+            steam.SetActive(false);
+        }
+    }
+}
