b229f6f864dad9d0bb4980364cb9b7114eb56c28
Roman <nosferat@bk.ru>
2017-04-25 19:52:17 +0300

    New navigation system in select level menu. Add sounds to menu

---
+++ Assets/Scripts/UI/Menu/PickLevelButton.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class PickLevelButton : MonoBehaviour {
+    public Level level;
+    private Image image;
+    private Button button;
+    private Text number;
+    private SelectLevelMenu menu;
+    private Button selectLevelPack;
+    private Button next;
+    private Button previous;
+
+    void Awake() {
+        menu = GameObject.Find("SelectLevelMenu").GetComponent<SelectLevelMenu>();
+        selectLevelPack = GameObject.Find("LevelPack").GetComponent<Button>();
+        image = GetComponent<Image>();
+        button = GetComponent<Button>();
+        number = GetComponentInChildren<Text>();
+    }
+
+    void Start() {
+        number.text = level.number.ToString();
+
+    }
+
+    public void OnClick() {
+        EventSystem eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
+        eventSystem.SetSelectedGameObject(GameObject.Find("Play"));
+        AudioManager.instance.PlayClick();
+    }
+
+    public void AddNextPrevButtons(Button next, Button previous) {
+        this.next = next;
+        this.previous = previous;
+    }
+
+    private void ChangeLevelPackButtonNavigation() {
+        Navigation navigation = selectLevelPack.navigation;
+        navigation.mode = Navigation.Mode.Explicit;
+        navigation.selectOnDown = button;
+        navigation.selectOnUp = button;
+        selectLevelPack.navigation = navigation;
+    }
+
+    public void OnSelect() {
+        menu.ActiveLevel(this);
+        level.BuildPart();
+        image.fillCenter = true;
+        Navigation navigation = button.navigation;
+        navigation.mode = Navigation.Mode.Explicit;
+        navigation.selectOnUp = selectLevelPack;
+        navigation.selectOnDown = selectLevelPack;
+        navigation.selectOnLeft = previous;
+        navigation.selectOnRight = next;
+        button.navigation = navigation;
+        ChangeLevelPackButtonNavigation();
+        AudioManager.instance.PlaySelect();
+    }
+
+    public void Deactivate() {
+        image.fillCenter = false;
+        Navigation navigation = button.navigation;
+        navigation.mode = Navigation.Mode.None;
+        button.navigation = navigation;
+    }
+}
--- Assets/Scripts/UI/Menu/SelectLevelMenu.cs
+++ Assets/Scripts/UI/Menu/SelectLevelMenu.cs
@@ -1,25 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
 // Sory for many stupid comments, i am tried to refactor this class
 public class SelectLevelMenu : Menu {
     private Pack currentPack;
-    private Level currentLevel;
-    public RectTransform levelsView;
-    public Button play;
-    public Button select;
-    public Button back;
-    public Text levelPackName;
+    public Level currentLevel;
+    public Text levelPack;
     public RectTransform levelsPanel;
-    public RectTransform levelButton;
-    private Image currentLevelSelect;
+    public RectTransform levelButtonPrefab;
+    private PickLevelButton selectedLevel;
 
     // If player click "Start" in Main Menu this menu is enable
     void OnEnable() {
         Camera.main.GetComponent<OrbitCamera>().isRotate = true;
         currentPack = LevelManager.GetCurrentPack();
-        BuildLevel(FirstLevelInPack());
+        LoadLevelList();
     }
 
     // If player click "Back" in Select Level Menu this menu is disable
@@ -30,14 +27,13 @@ public class SelectLevelMenu : Menu {
     // Load first level from pack in future must load last incomplete level get from profile
     public void Play() {
         if (currentLevel == null)
-            currentLevel = FirstLevelInPack();
+            currentLevel = currentPack.GetFirstLevelInPack();
         GameManager.level = currentLevel;
         GameManager.instance.StartGame();
     }
 
-    // Activate panel with level list
     public void SelectLevel() {
-        SelectActivation(!levelsView.gameObject.activeSelf);
+        // This method implement in inspector
     }
 
     // Return to Main Menu
@@ -46,17 +42,8 @@ public class SelectLevelMenu : Menu {
         LevelBuilder.DestroyLevel();
     }
 
-    private void SelectActivation(bool active) {
-        levelsView.gameObject.SetActive(active);
-        play.interactable = !active;
-        back.interactable = !active;
-        if (active) {
-            LoadLevelList();
-        }
-    }
-
     // Change pack, set current level pack and load button list for pick level
-    public void ChangeLevelPack() {
+    public void SwitchLevelPack() {
         currentPack = LevelManager.SwitchLevelPack();
         LoadLevelList();
     }
@@ -64,24 +51,39 @@ public class SelectLevelMenu : Menu {
     private void LoadLevelList() {
         SetLevelPackName();
         ClearLevelList();
-        bool isLoaded = false;
         IDictionary<int, Level> levels = currentPack.GetLevels();
+        List<Button> selectButtons = new List<Button>();
         foreach (var level in levels.Values) {
-            var changeButton = Instantiate(levelButton, levelsPanel);
-            Text text = changeButton.FindChild("Number").GetComponent<Text>();
-            Image image = changeButton.GetComponent<Image>();
-            Button button = changeButton.GetComponent<Button>();
-            text.text = level.number.ToString();
-            AddListenerToButton(button, level, image);
-            if (!isLoaded) {
-                ChangeLevel(level, image);
-                isLoaded = true;
+            var button = Instantiate(levelButtonPrefab, levelsPanel).GetComponent<Button>();
+            button.GetComponent<PickLevelButton>().level = level;
+            selectButtons.Add(button);
+        }
+        LinkButtons(selectButtons);
+        ActivateFirstButton(selectButtons.First());
+    }
+
+    private void LinkButtons(List<Button> buttons) {
+        int count = buttons.Count;
+        for (int i = 0; i < buttons.Count; i++) {
+            var pick = buttons[i].GetComponent<PickLevelButton>();
+            if (i == 0) {
+                pick.AddNextPrevButtons(buttons[i + 1],
+                    buttons[count - 1]);
+            }
+            else if (i == count - 1) {
+                pick.AddNextPrevButtons(buttons[0],
+                    buttons[i - 1]);
+            }
+            else {
+                pick.AddNextPrevButtons(buttons[i + 1],
+                    buttons[i - 1]);
             }
         }
     }
 
-    private void AddListenerToButton(Button button, Level level, Image image) {
-        button.onClick.AddListener(delegate { ChangeLevel(level, image); });
+    private void ActivateFirstButton(Button first) {
+        var pick = first.GetComponent<PickLevelButton>();
+        pick.OnSelect();
     }
 
     private void ClearLevelList() {
@@ -92,23 +94,14 @@ public class SelectLevelMenu : Menu {
     }
 
     private void SetLevelPackName() {
-        levelPackName.text = currentPack.packName;
+        levelPack.text = currentPack.packName;
     }
 
-    public void ChangeLevel(Level level, Image image) {
-        if (currentLevelSelect != null)
-            currentLevelSelect.fillCenter = false;
-        currentLevelSelect = image;
-        currentLevelSelect.fillCenter = true;
-        currentLevel = level;
-        BuildLevel(currentLevel);
-    }
-
-    private Level FirstLevelInPack() {
-        return currentPack.GetFirstLevelInPack();
-    }
-
-    private void BuildLevel(Level level) {
-        level.BuildPart();
+    public void ActiveLevel(PickLevelButton button) {
+        if (selectedLevel != null) {
+            selectedLevel.Deactivate();
+        }
+        currentLevel = button.level;
+        selectedLevel = button;
     }
 }
